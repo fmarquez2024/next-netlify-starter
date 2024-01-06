2a2a7ec04ff56e4056383f4221c2d9bedb1059ef
fmarquez2024 <155827936+fmarquez2024@users.noreply.github.com>
2024-01-06 10:42:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,29 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+<script type='text/javascript'>
+	function initEmbeddedMessaging() {
+		try {
+			embeddedservice_bootstrap.settings.language = 'es_MX'; // For example, enter 'en' or 'en-US'
+
+			embeddedservice_bootstrap.init(
+				'00DO1000000XAvE',
+				'Mensajeria_Web',
+				'https://socovesa--devmsys.sandbox.my.site.com/ESWMensajeriaWeb1701890481828',
+				{
+					scrt2URL: 'https://socovesa--devmsys.sandbox.my.salesforce-scrt.com'
+				}
+			);
+		} catch (err) {
+			console.error('Error loading Embedded Messaging: ', err);
+		}
+	};
+</script>
+<script type='text/javascript' src='https://socovesa--devmsys.sandbox.my.site.com/ESWMensajeriaWeb1701890481828/assets/js/bootstrap.min.js' onload='initEmbeddedMessaging()'></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+
+    
       </main>
 
       <Footer />
